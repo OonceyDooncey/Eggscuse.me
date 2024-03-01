067152e6b16993fd95577600fafc6f9a2ac238ce
OonceyDooncey <sohenqi000@gmail.com>
2024-03-01 19:17:15 +0800

    [feat]: Added page transition

--- app.vue
+++ app.vue
@@ -5,3 +5,19 @@
     </NuxtLayout>
   </div>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+
+.page-leave-to {
+  filter: blur(1rem);
+  transform: translateX(-1500px);
+}
+
+.page-enter-from {
+  transform: translateX(1000px)
+}
+</style>
