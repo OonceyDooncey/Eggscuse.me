eb35dfddcc74835eacbf49a3fa50ebcb01583cf5
OonceyDooncey <sohenqi000@gmail.com>
2024-03-03 15:59:29 +0800

    [feat]: Added thumbnail for buttons

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,31 @@
 <template>
-  <div class="h-full flex flex-col items-center">
-    <p
-      class="font-poppins text-md md:h-1/6 text-center mt-2 md:text-xl lg:text-2xl"
-    >
-      Your one stop platform for eggscellent eggscuses
-    </p>
-    <div class="font-margarine mt-5 grid grid-cols-1 md:grid-cols-3 gap-5">
-      <NuxtLink to="/search/work" class="option-btn">
-        <span class="option">Work</span>
-      </NuxtLink>
-      <NuxtLink to="/search/school" class="option-btn">
-        <span class="option">School</span>
-      </NuxtLink>
-      <NuxtLink to="/search/social" class="option-btn">
-        <span class="option">Social</span>
-      </NuxtLink>
+    <div class="h-full flex flex-col items-center">
+        <p
+            class="font-poppins text-md md:h-1/6 text-center mt-2 md:text-xl lg:text-2xl">
+            Your one stop platform for eggscellent eggscuses
+        </p>
+        <div class="font-margarine mt-5 grid grid-cols-1 md:grid-cols-3 gap-5">
+            <NuxtLink to="/search/work" class="option-btn">
+                <img
+                    src="~/assets/work_egg.png"
+                    alt="egg thumbnail"
+                    class="option-img" />
+                <span class="option">Work</span>
+            </NuxtLink>
+            <NuxtLink to="/search/school" class="option-btn">
+                <img
+                    src="~/assets/school_egg.png"
+                    alt="egg thumbnail"
+                    class="option-img" />
+                <span class="option">School</span>
+            </NuxtLink>
+            <NuxtLink to="/search/social" class="option-btn">
+                <img
+                    src="~/assets/social_egg.png"
+                    alt="egg thumbnail"
+                    class="option-img" />
+                <span class="option">Social</span>
+            </NuxtLink>
+        </div>
     </div>
-  </div>
 </template>
