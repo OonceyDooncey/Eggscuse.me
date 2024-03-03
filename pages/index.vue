fac32ef90ca7fba640c783a334d034489ec3e56c
OonceyDooncey <sohenqi000@gmail.com>
2024-03-03 17:24:51 +0800

    [style]: Added hover effect for buttons

--- pages/index.vue
+++ pages/index.vue
@@ -5,21 +5,27 @@
             Your one stop platform for eggscellent eggscuses
         </p>
         <div class="font-margarine mt-5 grid grid-cols-1 md:grid-cols-3 gap-5">
-            <NuxtLink to="/search/work" class="option-btn">
+            <NuxtLink
+                to="/search/work"
+                class="option-btn md:hover:border-gray-950 transition">
                 <img
                     src="~/assets/work_egg.png"
                     alt="egg thumbnail"
                     class="option-img" />
                 <span class="option">Work</span>
             </NuxtLink>
-            <NuxtLink to="/search/school" class="option-btn">
+            <NuxtLink
+                to="/search/school"
+                class="option-btn md:hover:border-gray-950 transition">
                 <img
                     src="~/assets/school_egg.png"
                     alt="egg thumbnail"
                     class="option-img" />
                 <span class="option">School</span>
             </NuxtLink>
-            <NuxtLink to="/search/social" class="option-btn">
+            <NuxtLink
+                to="/search/social"
+                class="option-btn md:hover:border-gray-950 transition">
                 <img
                     src="~/assets/social_egg.png"
                     alt="egg thumbnail"
