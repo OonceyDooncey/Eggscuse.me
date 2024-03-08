b97aab39d4bd2e4587eadc88296a5d7e0c0baa62
OonceyDooncey <sohenqi000@gmail.com>
2024-03-08 15:28:00 +0800

    [feat]: Updated SEO

--- app.vue
+++ app.vue
@@ -1,23 +1,48 @@
+<script setup lang="ts">
+useSeoMeta({
+    title: "Eggscuse.me",
+    ogTitle: "Eggscuse.me",
+    description:
+        "Excuse Generator, your one stop platform for eggscellent eggscuses.",
+    ogDescription:
+        "Excuse Generator, your one stop platform for eggscellent eggscuses.",
+    ogImage: "/ogImage.jpg",
+    twitterCard: "summary_large_image",
+});
+useHead({
+    htmlAttrs: {
+        lang: "en",
+    },
+    link: [
+        {
+            rel: "icon",
+            type: "image/png",
+            href: "/favicon.png",
+        },
+    ],
+});
+</script>
+
 <template>
-  <div>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </div>
+    <div>
+        <NuxtLayout>
+            <NuxtPage />
+        </NuxtLayout>
+    </div>
 </template>
 
 <style>
 .page-enter-active,
 .page-leave-active {
-  transition: all 0.4s;
+    transition: all 0.4s;
 }
 
 .page-leave-to {
-  filter: blur(1rem);
-  transform: translateX(-1500px);
+    filter: blur(1rem);
+    transform: translateX(-1500px);
 }
 
 .page-enter-from {
-  transform: translateX(1000px)
+    transform: translateX(1000px);
 }
 </style>
