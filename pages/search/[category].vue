4f7e57160f95d5676419b1f8f4d2e5dde4439ae2
OonceyDooncey <sohenqi000@gmail.com>
2024-03-10 17:39:50 +0800

    [feat]: Added loader when generating new excuse

--- pages/search/[category].vue
+++ pages/search/[category].vue
@@ -9,15 +9,28 @@
                 class="option-img" />
             <span class="option">{{ category }}</span>
         </div>
-        <div class="mt-14">
-            <p class="text-xl text-center md:text-2xl font-poppins">
+        <div class="mt-14 flex flex-col items-center relative">
+            <div
+                class="w-fit bg-title p-2 rounded-2xl flex absolute items-center"
+                ref="loaderContainer">
+                <div class="loader"></div>
+                <span
+                    class="text-lg text-center md:text-2xl font-poppins block text-white">
+                    Generating...
+                </span>
+            </div>
+            <p
+                class="text-xl text-center md:text-2xl font-poppins h-24 md:h-10">
                 {{ excuse }}
             </p>
         </div>
         <div class="grid grid-cols-2 mt-10 gap-5">
             <button
                 class="p-2 bg-gray-950 rounded-lg"
-                @click="fetchExcuse(category)">
+                @click="
+                    fetchExcuse(category);
+                    fetchingData(true);
+                ">
                 <NuxtImg
                     format="webp"
                     src="/icon/restart.svg"
@@ -40,6 +53,7 @@
 <script setup lang="ts">
 const { category } = useRoute().params;
 const excuse = ref("");
+const loaderContainer = ref();
 
 const fetchExcuse = debounce(async (selectedCategory: string | string[]) => {
     //fetch data for the excuse
@@ -61,6 +75,7 @@ const fetchExcuse = debounce(async (selectedCategory: string | string[]) => {
             return;
         }
 
+        fetchingData(false);
         excuse.value = data;
     } catch (error) {
         console.error("Error:", error);
@@ -68,6 +83,15 @@ const fetchExcuse = debounce(async (selectedCategory: string | string[]) => {
     }
 }, 1000);
 fetchExcuse(category);
+
+const fetchingData = (fetching: boolean) => {
+    if (fetching === false) {
+        loaderContainer.value.classList.add("loaded");
+    } else {
+        loaderContainer.value.classList.remove("loaded");
+        excuse.value = "";
+    }
+};
 
 const doShowError = (message: string) => {
     showError({
@@ -112,3 +136,37 @@ if (category === "social") {
     url.value = "/social_egg.png";
 }
 </script>
+
+<style scoped>
+.loader {
+    width: 35px;
+    padding: 6px;
+    aspect-ratio: 1;
+    border-radius: 50%;
+    background: white;
+    --_m: conic-gradient(#0000 10%, #000), linear-gradient(#000 0 0) content-box;
+    -webkit-mask: var(--_m);
+    mask: var(--_m);
+    -webkit-mask-composite: source-out;
+    mask-composite: subtract;
+    animation: loading 1s infinite linear;
+    margin-right: 0.5rem;
+}
+
+.loaded {
+    animation: fade-out 0.1s forwards linear;
+}
+
+@keyframes loading {
+    to {
+        transform: rotate(1turn);
+    }
+}
+
+@keyframes fade-out {
+    to {
+        opacity: 0;
+        display: none;
+    }
+}
+</style>
